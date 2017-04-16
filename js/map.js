7e733d55aaba3268a7393be1a63948da478ed7b5
[Martina Klima] <[martinaklimova0@gmail.com]>
2017-04-16 16:02:12 +0200

    Created Folders / CSS/ Customized css and downloaded Bootsrap theme. JS/ created map.js with function initMap() created index.html which loads map

---
+++ js/map.js
@@ -0,0 +1,10 @@
+
+
+function initMap() {
+var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 50.08804, lng: 14.42076},
+          zoom: 14,
+
+        });
+
+}
