64e3923f303b7e57d161ab88decd5f80016b67f2
[Martina Klima] <[martinaklimova0@gmail.com]>
2017-04-24 11:15:50 +0200

    Merge branch 'master' into boot
    # Conflicts:
    #	index.html
    #	js/map.js

--- js/map.js
+++ js/map.js
@@ -1,6 +1,16 @@
+<<<<<<< HEAD
 var viewModel;
 var map;
 var marker;
+||||||| merged common ancestors
+
+=======
+
+var viewModel;
+var map;
+var markers = [];
+var myInfowindow;
+>>>>>>> master
 
 function initMap() {
 
@@ -118,7 +128,7 @@ function initMap() {
         }
       ],
       {name: 'Styled Map'});
-        var map = new google.maps.Map(document.getElementById('map'), {
+          map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: 50.08804, lng: 14.42076},
           zoom: 14,
           zoomControl: true,
@@ -133,28 +143,80 @@ function initMap() {
 
 
   });
-
+  function googleError() {
+    alert("Failed to download Google Maps data.");
+}
   //Associate the styled map with the MapTypeId and set it to display.
   map.mapTypes.set('styled_map', styledMapType);
   map.setMapTypeId('styled_map');
+<<<<<<< HEAD
   console.log("initMap executed");
   viewModel.google(!!window.google);
+||||||| merged common ancestors
+=======
+  console.log("initMap executed");
+  viewModel.google(!!window.google);
+  myInfowindow = new google.maps.InfoWindow();
+>>>>>>> master
 }
 
+
 var initialBars = [
-          {title: 'EMA espresso bar', location: {lat: 50.088707, lng: 14.433490}, zIndex: 1},
-          {title: 'Onesip coffee', location: {lat: 50.091282, lng: 14.425393}, zIndex: 2},
-          {title: 'Styl&Interier', location: {lat: 50.081620, lng: 14.424524}, zIndex: 3},
-          {title: 'Café jen', location: {lat: 50.071433, lng: 14.455954}, zIndex: 4},
-          {title: 'Místo', location: {lat: 50.099013, lng: 14.404463}, zIndex: 5},
-          {title: 'Kavárna co hledá jméno', location: {lat: 50.069659, lng: 14.403759}, zIndex: 6}
+          {title: 'EMA espresso bar', location: {lat: 50.088707, lng: 14.433490}, types: 'bar'},
+          {title: 'Onesip coffee', location: {lat: 50.091282, lng: 14.425393}, types: 'coffee'},
+          {title: 'Styl&Interier', location: {lat: 50.081620, lng: 14.424524}, types: 'bar'},
+          {title: 'Café jen', location: {lat: 50.071433, lng: 14.455954}, types: 'bar'},
+          {title: 'Místo', location: {lat: 50.099013, lng: 14.404463}, types: 'coffee'},
+          {title: 'Kavárna co hledá jméno', location: {lat: 50.069659, lng: 14.403759}, types: 'coffee', id: "56c9fc28cd104596fc266354"}
         ];
+<<<<<<< HEAD
 var Bar = function(data, vm) {
     this.title = data.title;
     this.location = data.location;
     this.lat = data.location.lat;
     this.lng = data.location.lng;
+||||||| merged common ancestors
+=======
+  //Setting up Foursquare for infowindow
+var CLIENT_ID = 'YGBWYBRYGYG42BAT3E3HL0A5LKYITIYNUR52BQDBXQPUI15D';
+var CLIENT_SECRET = 'YGBWYBRYGYG42BAT3E3HL0A5LKYITIYNUR52BQDBXQPUI15D';
 
+
+
+
+var Bar = function(data, vm) {
+    var self = this;
+    this.title = data.title;
+    this.location = data.location;
+    this.lat = data.location.lat;
+    this.lng = data.location.lng;
+    this.types = data.types;
+    this.id = data.id;
+
+    self.makeMarker = ko.computed(function() {
+      console.log(vm.google());
+      if (vm.google()) { // if (vm.google() === true) {
+         // create marker
+        self.marker = new google.maps.Marker({
+        title: data.title,
+        position: data.location,
+        map: map,
+        animation: google.maps.Animation.DROP
+
+    });
+        self.marker.addListener('click', function() {
+        self.openInfoWindow();
+        self.toggleBounce();
+    });
+
+        var contentString = "Hello";
+        self.openInfoWindow = function(data) {
+        self.myInfowindow.open(map, self.marker);
+        self.myInfowindow.setContent(contentString);
+        };
+>>>>>>> master
+
+<<<<<<< HEAD
     // http://knockoutjs.com/documentation/computedObservables.html
     self.makeMarker = ko.computed(function() {
       console.log(vm.google());
@@ -164,17 +226,58 @@ var Bar = function(data, vm) {
         position: data.location,
         map: map,
         animation: google.maps.Animation.DROP
+||||||| merged common ancestors
+var Bar = function(data) {
+    this.title = ko.observable(data.title);
+=======
+        self.toggleBounce = function() {
+        self.marker.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(function () {
+            self.marker.setAnimation(null);
+        }, 1200);
+      };
+>>>>>>> master
 
     })
   };
 });
 }
+<<<<<<< HEAD
 
+||||||| merged common ancestors
+=======
+});
+};
+
+
+
+
+>>>>>>> master
 var ViewModel = function() {
+<<<<<<< HEAD
    console.log("ViewModel instantiated");
        var self = this;
 
     this.google = ko.observable(!!window.google); // false
+||||||| merged common ancestors
+    var self = this;
+=======
+   console.log("ViewModel instantiated");
+       var self = this;
+
+    this.google = ko.observable(!!window.google); // false
+    this.userQuery = ko.observable("");
+    this.printoToConsole = ko.computed(function() {
+    console.log(self.userQuery());
+    });
+
+    self.search = ko.computed (function () {
+    var userQuery = self.userQuery().toLowerCase();
+
+    console.log('userQuery: ', userQuery);
+
+    });
+>>>>>>> master
     this.barList = ko.observableArray([]);
     initialBars.forEach(function(barItem) {
     self.barList.push(new Bar(barItem, self) );
